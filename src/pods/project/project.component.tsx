e663c45bd74c84e4a1b550d9e7be8ca730df6f44
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2020-04-27 16:26:11 +0200

    Refactor components

--- src/pods/project/project.component.tsx
+++ src/pods/project/project.component.tsx
@@ -1,21 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { routes } from 'core/router';
 
 export const ProjectComponent: React.FunctionComponent = () => {
-  const history = useHistory();
-  const goToProject = (
-    event: React.MouseEvent<HTMLParagraphElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-    history.push({
-      pathname: routes.editProject('1'),
-    });
-  };
-  return (
-    <>
-      <h1>Hello Edit project component</h1>
-      <p onClick={goToProject}>Go to edit project component</p>
-    </>
-  );
+  return <h1>Hello Edit project component</h1>;
 };
