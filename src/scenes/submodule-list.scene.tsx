8dd5fb6c6cf4fe47d7a19f866f5e45a7041c5da2
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2020-04-28 16:41:25 +0200

    Created submodule list container and refactor submodule list component

--- src/scenes/submodule-list.scene.tsx
+++ src/scenes/submodule-list.scene.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AppLayout } from 'layouts';
-import { SumoduleListComponent } from 'pods/submodule-list';
+import { SubmoduleListContainer } from 'pods/submodule-list';
 
 export const SubmoduleListScene: React.FC = () => {
   return (
     <AppLayout>
-      <SumoduleListComponent />
+      <SubmoduleListContainer />
     </AppLayout>
   );
 };
