d38a2b895c4c41cae42ab262b0711db60455460c
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2020-05-04 16:32:38 +0200

    Add select common component and create data styles

--- src/pods/employee/components/data.component.tsx
+++ src/pods/employee/components/data.component.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Formik, Form } from 'formik';
+import { cx } from 'emotion';
+import Button from '@material-ui/core/Button';
+import { TextFieldComponent, CheckboxComponent } from 'common/components';
+import * as classes from './data.styles';
 
 export const DataComponent: React.FunctionComponent = () => {
-  return <h1>Hello Data component</h1>;
+  return (
+    <Formik
+      initialValues={{}}
+      enableReinitialize={true}
+      onSubmit={console.log}
+      validate={Promise.resolve}
+    >
+      {({ values }) => (
+        <Form className={classes.form}>
+          <TextFieldComponent label="Id" name="id" />
+          <TextFieldComponent label="Clave Temporal" name="temporalKey" />
+          <TextFieldComponent label="Nombre" name="name" />
+          <TextFieldComponent label="Email" name="email" />
+          <CheckboxComponent name="active" label="Activo" color="primary" />
+          <div className={classes.buttonContainer}>
+            <Button variant="contained" color="primary">
+              Cancelar
+            </Button>
+            <Button variant="contained" color="primary" onClick={console.log}>
+              Grabar
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 };
---
+++ src/pods/employee/components/data.styles.ts
@@ -0,0 +1,15 @@
+import { css } from 'emotion';
+
+export const form = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+`;
+
+export const buttonContainer = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 1rem;
+`;
