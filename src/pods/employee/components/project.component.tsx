e286d56de9913abaf7742869c2097077b8398d83
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2020-05-04 13:04:47 +0200

    Create data, project and report component

---
+++ src/pods/employee/components/project.component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ProjectComponent: React.FunctionComponent = () => {
+  return <h1>Hello Project component</h1>;
+};
--- src/pods/employee/employee.component.tsx
+++ src/pods/employee/employee.component.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import {
+  TabComponent,
+  TabListComponent,
+  TabPanelComponent,
+} from 'common/components';
+import { DataComponent, ProjectComponent, ReportComponent } from './components';
 
 export const EmployeeComponent: React.FunctionComponent = () => {
-  return <h1>Hello Edit employee component</h1>;
+  const [tab, setTab] = React.useState(0);
+  return (
+    <>
+      <TabListComponent value={tab} onChange={setTab}>
+        <TabComponent label="Datos" />
+        <TabComponent label="Proyectos" />
+        <TabComponent label="Informes" />
+      </TabListComponent>
+      <TabPanelComponent value={tab} index={0}>
+        <DataComponent />
+      </TabPanelComponent>
+      <TabPanelComponent value={tab} index={1}>
+        <ProjectComponent />
+      </TabPanelComponent>
+      <TabPanelComponent value={tab} index={2}>
+        <ReportComponent />
+      </TabPanelComponent>
+    </>
+  );
 };
